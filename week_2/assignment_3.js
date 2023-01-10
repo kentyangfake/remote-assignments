d769a1a61e01b983df0849d1d4b4ffe3576e90b3
YuChien Yang <yuuchien.yang@gmail.com>
2023-01-10 22:39:28 +0800

    assignment1.2.3 done

---
+++ week_2/assignment_3.js
@@ -0,0 +1,26 @@
+function calculate(data) {
+    let discount = data.discount;
+    let total = 0;
+    for(let product of data.products){
+        total += product.price;
+    }
+    return total*discount; 
+}
+    const discountedPrice = calculate({
+    discount: 0.1,
+    products: [
+    {
+    name: "Product 1",
+    price: 100
+    },
+    {
+    name: "Product 2",
+    price: 700
+    },
+    {
+    name: "Product 3",
+    price: 250
+    }
+    ]
+    });
+    console.log(discountedPrice)
