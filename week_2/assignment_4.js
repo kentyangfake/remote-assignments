bbc81ad89a0dcaefa77be8866d4b38b337e0b43c
YuChien Yang <yuuchien.yang@gmail.com>
2023-01-10 22:05:42 +0800

    day2 progress

--- week_2/assignment_4.js
+++ week_2/assignment_4.js
@@ -1,2 +1,8 @@
-const addColumns = document.getElementsById('btn-addColumns');
-addEventListener(,()=>{});
+const greetingBanner = document.getElementById("welcome-banner");
+
+
+greetingBanner.addEventListener('click', () => {
+    console.log('clicked!');
+    let greeting = document.getElementsByTagName('h2')[0];
+    greeting.textContent = 'Have a good Time!';
+});
