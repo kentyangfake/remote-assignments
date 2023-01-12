d9d45abc52c2198f21a5effa0fd68da7279e052a
YuChien Yang <yuuchien.yang@gmail.com>
2023-01-12 17:51:28 +0800

    get ref for assignment_4

--- week_2/assignment_4.js
+++ week_2/assignment_4.js
@@ -1,5 +1,14 @@
-const greetingBanner = document.getElementById("welcome-banner");
+let hamburger = document.querySelector('.hamburger');
+let xmark = document.querySelector('.xmark');
+let sideMenu = document.querySelector('.side-menu');
+    
+hamburger.addEventListener('click', () => {
+    hamburger.classList.add("hide");
+});
 
+
+
+const greetingBanner = document.getElementById("welcome-banner");
 
 greetingBanner.addEventListener('click', () => {
     console.log('clicked!');
